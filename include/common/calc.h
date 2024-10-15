872e22d7a0f68e3841ceccc315dc448b966b14e6
Joshua Chen <jhc3628@gmail.com>
2024-10-15 12:36:10 -0500

    Fix

--- include/common/calc.h
+++ include/common/calc.h
@@ -4,9 +4,9 @@ static const double H_STEP = 0.01;
 
 template<typename T>
 double integrate(double start, double end, T function) {
-      double sum = 0;
+    double sum = 0;
     for (double x = start; x <= end; x+=H_STEP) {
         sum += function(x);
     }
-    return sum*0.01;
+    return sum*H_STEP;
 }
