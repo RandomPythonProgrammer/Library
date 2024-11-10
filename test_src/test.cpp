80e938831e429c3cadef2b9658f53c3201036612
Joshua Chen <jhc3628@gmail.com>
2024-11-10 09:41:11 -0600

    Fixed backwards paths

--- test_src/test.cpp
+++ test_src/test.cpp
@@ -12,10 +12,11 @@
 TEST_CASE("Visualizer Spline", "[Spline]") {
     Pose2d start{{0, 0}, 0};
     std::shared_ptr<Trajectory> path = TrajectoryBuilderFactory::create(start)
-        .to({{10, 10}, 0.25})
-        .to({{20, 10}, 0})
+        .to({{-10, -10}, 0.25})
+        .to({{-20, -10}, 0})
         .build();
 
     Pose2d pose = poseByArcLength(*path, 16);
-    REQUIRE(std::abs(path->splines[0].length + arcLength(path->splines[1].coefficients, path->splines[1].start.position.x(), pose.position.x()) - 16) < ERROR);
+    double length = arcLength(path->splines[1].coefficients, path->splines[1].start.position.x(), pose.position.x());
+    REQUIRE(std::abs(path->splines[0].length + length - 16) < ERROR);
 }
